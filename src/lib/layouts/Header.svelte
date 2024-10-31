e766805af0f620a31f6b0c2304ce3383ce3290d1
Henrique Ramos <henriquealberone@outlook.com>
2024-10-31 12:27:37 -0300

    feat: add header

---
+++ src/lib/layouts/Header.svelte
@@ -0,0 +1,26 @@
+<header>
+	<h2 class="logo">Workout</h2>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/workouts">Workouts</a>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: calc(var(--base-spacing) * 2);
+	}
+
+	.logo {
+		color: var(--text-high);
+	}
+
+	nav {
+		color: var(--color-primary);
+		display: flex;
+		gap: 1rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import { invalidate } from '$app/navigation';
 	import { onMount, type Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
+	import Header from '$lib/layouts/Header.svelte';
+	import '$lib/theme/index.css';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 	let { session, supabase } = data;
@@ -10,9 +12,17 @@
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
 			if (newSession?.expires_at !== session?.expires_at) invalidate('supabase:auth');
 		});
-
 		return () => data.subscription.unsubscribe();
 	});
 </script>
 
-{@render children()}
+<Header />
+<div>
+	{@render children()}
+</div>
+
+<style>
+	div {
+		padding: calc(var(--base-spacing) * 2);
+	}
+</style>
