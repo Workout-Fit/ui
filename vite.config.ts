ca18d2da0d49bd019e0e9672038f4362dc9b9fa9
Henrique Ramos <henriquealberone@outlook.com>
2024-11-01 11:19:16 -0300

    chore: add @testing-library/svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()],
+	plugins: [
+		sveltekit(),
+		svelteTesting(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			injectRegister: false,
+
+			pwaAssets: {
+				disabled: false,
+				config: true
+			},
+
+			manifest: {
+				name: 'Workout',
+				short_name: 'Workout',
+				description: 'Workout',
+				theme_color: '#ffffff'
+			},
+
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true
+			},
+
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module'
+			}
+		})
+	],
 
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		setupFiles: ['./vitest-setup.js'],
+		include: ['tests/**/*.spec.ts'],
+		environment: 'happy-dom'
 	}
 });
