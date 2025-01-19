fb05d84372678eb3ba441580077089937d80ccd1
Henrique Ramos <henriquealberone@outlook.com>
2025-01-18 22:19:58 -0300

    chore: add i18n message for Exercise rest

--- src/lib/components/ExerciseListItem.svelte
+++ src/lib/components/ExerciseListItem.svelte
@@ -2,13 +2,14 @@
 	import type { WorkoutExercise } from '$lib/types';
 	import type { Snippet } from 'svelte';
 	import ListItem from './ListItem.svelte';
+	import * as m from '$lib/paraglide/messages.js';
 
 	let { exercise, decoration }: { exercise: WorkoutExercise; decoration?: Snippet } = $props();
 </script>
 
 <ListItem
 	title={exercise.exercise.i18n[0].name}
-	secondLine={`${exercise.sets}×${exercise.repetitions}${exercise.rest !== undefined ? `, rest for ${exercise.rest}"` : ''}`}
+	secondLine={`${exercise.sets}×${exercise.repetitions}${exercise.rest !== undefined ? `, ${m.rest_for({ rest: exercise.rest })}"` : ''}`}
 	thirdLine={exercise.notes ?? undefined}
 	rightDecoration={decoration}
 />
